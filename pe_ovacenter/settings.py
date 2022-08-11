024b4fb29f76342e61575ee359fefdc01a7e0a1f
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-11 06:42:50 -0500

    agregado setting

--- pe_ovacenter/settings.py
+++ pe_ovacenter/settings.py
@@ -69,6 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'pe_ovacenter.urls'
