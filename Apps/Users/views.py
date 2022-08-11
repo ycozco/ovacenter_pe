2eb2b01aa172115a72b1a44eceee41fbb44487fc
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-11 09:39:52 -0500

    view

--- Apps/Users/views.py
+++ Apps/Users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.core import serializers
 from rest_framework import viewsets
-from .models import ClienteSerializer,Cliente
+from .models import ClienteSerializer,Cliente,Instructor,InstructorSerializer
 from django.http import HttpResponse
 # Create your views here.
 class ClienteViewSet(viewsets.ModelViewSet):
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
+class InstructorViewSet(viewsets.ModelViewSet):
+    queryset = Instructor.objects.all()
+    serializer_class =InstructorSerializer
