1d2b69fcea2256f91bbba9f9631dbd9c1dfada7b
Vladimir2003-debug <83074130+Vladimir2003-debug@users.noreply.github.com>
2022-08-11 07:03:34 -0500

    Update settings.py

--- pe_ovacenter/settings.py
+++ pe_ovacenter/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Enable WhiteNoise's GZip compression of static assets.
