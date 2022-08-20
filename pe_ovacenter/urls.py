afbbe73fb0d6e8835ecde26becc5726ab44527ab
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-20 18:23:14 -0500

    cambiado las rutas de las api

--- pe_ovacenter/urls.py
+++ pe_ovacenter/urls.py
@@ -19,8 +19,7 @@ from django.urls import path, include
 
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
-from Apps.Users.views import ClienteViewSet,InstructorViewSet
-from Apps.Eventos.views import EventosViewSet
+from api.views import EventosViewSet,ClienteViewSet,InstructorViewSet
 from inicio.views import Inicio
 
 router = routers.DefaultRouter()
