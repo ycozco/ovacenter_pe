193254bac6e9d51516330ce022484563b183e7db
Vladimir Arturo Sulla Quispe <vsullaq@unsa.edu.pe>
2022-08-20 18:22:04 -0500

    eventos normales

--- Apps/Eventos/views.py
+++ Apps/Eventos/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-from .models import EventoSerializer,Evento
-from rest_framework import viewsets
-from datetime import datetime
 
 # Create your views here.
-class EventosViewSet(viewsets.ModelViewSet):
-    queryset = Evento.objects.all()
-    serializer_class = EventoSerializer
-
 
 
